b9fad64378dfccbe5121a5a034f35f61043c178e
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-06-29 22:29:02 +0300

    Refactor event handling in app.py: implement create, update, and delete functionalities with proper JSON handling

--- app.py
+++ app.py
@@ -18,39 +18,51 @@ events = [
     Event(2, "Python Workshop")
 ]
 
-# TODO: Task 1 - Define the Problem
-# Create a new event from JSON input
 # Defines the end points
 @app.route("/events", methods=["POST"])
 def create_event():
-    # TODO: Task 2 - Design and Develop the Code
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    # Get info from the client to add it to our server
+    data = request.get_json()
+    #Increment the ID
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    new_event = Event( len(events)+ 1, data["title"] )
 
-# TODO: Task 1 - Define the Problem
-# Update the title of an existing event
+    events.append(new_event)
+    return jsonify(new_event.to_dict()), 201
+
+    
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
+    
+    # First see if it is there
+    event = next ((event for event in events if event.id == event_id), None)
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    data = request.get_json()
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    if not event:
+        return jsonify("Event not found"), 404
 
-# TODO: Task 1 - Define the Problem
-# Remove an event from the list
+    if "title" in data:
+        event.title =data["title"]
+    return jsonify(event.to_dict()), 200 
+
+
+
 @app.route("/events/<int:event_id>", methods=["DELETE"])
+#Use Id to get the event you want to delete first
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    event = next((event for event in events if event.id == event_id), None)
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    if not event:
+        return jsonify("Event not found"),404
+    
+    #filter the events that are there already using list compression
+    events[:] = [event for event in events if event.id != event_id]
 
+    return jsonify("Event deleted successfuly"),204
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
